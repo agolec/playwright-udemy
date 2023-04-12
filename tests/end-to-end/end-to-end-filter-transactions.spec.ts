9877490c893a0239a16dbc692b5f6fc6deb39482
Adam Golec <agolec@nexient.com>
2023-04-12 16:42:03 -0400

    added files into commit

--- tests/end-to-end/end-to-end-filter-transactions.spec.ts
+++ tests/end-to-end/end-to-end-filter-transactions.spec.ts
@@ -12,5 +12,19 @@ test.describe.only('Filter Transactions', () => {
     test('Verify the results for each account', async ({ page }) => {
         await page.goto('http://zero.webappsecurity.com/online-banking.html')
         await page.click('span#account_activity_link')
+        await page.selectOption('#aa_accountId', '2')
+        const checkingAccount = await page.locator(
+            '#all_transactions_for_account tbody tr'
+        )
+        await expect(checkingAccount).toHaveCount(3)
+        await page.selectOption('#aa_accountId', '4')
+        const loanAccount = await page.locator(
+            '#all_transactions_for_account tbody tr'
+        )
+        await expect(loanAccount).toHaveCount(2)
+
+        await page.selectOption('#aa_accountId', '6')
+        const brokerageAccount = await page.locator('.well')
+        await expect(brokerageAccount).toBeVisible()
     })
 })
