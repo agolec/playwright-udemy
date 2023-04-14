470fbb39ad7703068930c8a24c6ef4131f32f702
Adam Golec <agolec@nexient.com>
2023-04-14 14:20:59 -0400

    refactor submit-form.spec.ts to use FeedbackPage.ts

--- tests/end-to-end/end-to-end-search.spec.ts
+++ tests/end-to-end/end-to-end-search.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test'
 import { HomePage } from '../../page_objects/HomePage'
 
-test.describe.only('Search results', () => {
+test.describe('Search results', () => {
     test('Should find search results', async ({ page }) => {
         let homePage: HomePage = new HomePage(page)
         await homePage.visit()
         await homePage.searchFor('bank')
-      
+
         const numberOfLinks = await page.locator('li > a')
         await expect(numberOfLinks).toHaveCount(2)
     })
