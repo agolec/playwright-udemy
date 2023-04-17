1f9cc1c446e66c4310e51eea612763b2f99098d1
Adam Golec <agolec@nexient.com>
2023-04-17 16:55:39 -0400

    begin commenting out the existing test steps that were not using POM

--- tests/end-to-end/end-to-end-payment.spec.ts
+++ tests/end-to-end/end-to-end-payment.spec.ts
@@ -23,17 +23,17 @@ test.describe('New Payment', () => {
 
     test('Should send new payment', async ({ page }) => {
         await page.goto('http://zero.webappsecurity.com/online-banking.html')
-        await page.click('#pay_bills_link')
-        await page.selectOption('#sp_payee', 'apple')
-        await page.click('a#sp_get_payee_details')
-        await page.waitForSelector('#sp_payee_details')
-        await page.selectOption('#sp_account', '6')
+        // await page.click('#pay_bills_link')
+        //await page.selectOption('#sp_payee', 'apple')
+        // await page.click('a#sp_get_payee_details')
+        // await page.waitForSelector('#sp_payee_details')
+        // await page.selectOption('#sp_account', '6')
 
-        await page.type('input#sp_amount', '500')
-        await page.type('#sp_date', '2023-04-18')
-        await page.keyboard.press('Enter')
-        await page.type('#sp_description', 'some text')
-        await page.click('input#pay_saved_payees')
+        // await page.type('input#sp_amount', '500')
+        // await page.type('#sp_date', '2023-04-18')
+        // await page.keyboard.press('Enter')
+        // await page.type('#sp_description', 'some text')
+        // await page.click('input#pay_saved_payees')
 
         const message = await page.locator('#alert_content span')
         await expect(message).toBeVisible()
