761c9aadf5cac27a86588e0e71be66db8ef1b819
Adam Golec <agolec@nexient.com>
2023-04-17 17:32:10 -0400

    fix PaymentPage.ts locators.

--- page_objects/PaymentPage.ts
+++ page_objects/PaymentPage.ts
@@ -17,8 +17,8 @@ export class PaymentPage {
         this.payeeSelectBox = page.locator('#sp_payee')
         this.payeeDetailButton = page.locator('#sp_get_payee_details')
         this.payeeDetail = page.locator('a#sp_get_payee_details')
-        this.accountSelectBox = page.locator('#sp_payee_details')
-        this.amountInput = page.locator('#sp_account')
+        this.accountSelectBox = page.locator('#sp_account')
+        this.amountInput = page.locator('#sp_amount')
         this.dateInput = page.locator('#sp_date')
         this.descriptionInput = page.locator('#sp_description')
         this.submitPaymentButton = page.locator('input#pay_saved_payees')
@@ -29,5 +29,16 @@ export class PaymentPage {
         await this.payeeSelectBox.selectOption('apple')
         await this.payeeDetail.click()
         await expect(this.payeeDetail).toBeVisible()
+        await this.accountSelectBox.selectOption('6')
+        await this.amountInput.type('500')
+        await this.dateInput.type('2023-04-18')
+        await this.page.keyboard.press('Enter')
+        await this.submitPaymentButton.click()
+    }
+    async assertSuccessMessage() {
+        await expect(this.message).toBeVisible()
+        await expect(this.message).toContainText(
+            'The payment was successfully submitted.'
+        )
     }
 }
