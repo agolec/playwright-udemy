0e01220818e1aa1a1968649a5e56b8c6bd1b5b41
Adam <algolec1990@yahoo.com>
2023-04-24 22:32:18 -0400

    add two tests that assert response codes

---
+++ tests/api/api.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test'
+
+//setting up a test suite that runs api calls in parallel.
+//as each script will be using a different endpoint, these can all run in parallel of one another.
+test.describe.parallel('API Testing', () => {
+    //set up the root url our APIs will use.
+    const rootURL = 'https://reqres.in/api'
+    const GOOD = 200
+    const NOT_FOUND = 404
+    test('Simple API Test - Assert Response Status', async ({ request }) => {
+        const response = await request.get(`${rootURL}/users/2`)
+        expect(response.status()).toBe(GOOD)
+    })
+
+    test('Simple API Test - Assert Invalid Endpoint', async ({ request }) => {
+        const response = await request.get(`${rootURL}/users/non-existant-user`)
+        expect(response.status()).toBe(NOT_FOUND)
+    })
+})
