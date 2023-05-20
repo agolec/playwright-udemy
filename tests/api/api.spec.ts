f6faa12394215c23577dbf2871db594e965c0583
Adam <algolec1990@yahoo.com>
2023-05-19 23:13:27 -0400

    add api.spec.ts changes. status codes are in external file and imported. baseURL is also in external file and exported.

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -1,19 +1,32 @@
 import { test, expect } from '@playwright/test'
+import { GOOD, NOT_FOUND } from '../api/status.codes'
+import { baseURL } from '../api/base.url'
 
 //setting up a test suite that runs api calls in parallel.
 //as each script will be using a different endpoint, these can all run in parallel of one another.
 test.describe.parallel('API Testing', () => {
     //set up the root url our APIs will use.
-    const rootURL = 'https://reqres.in/api'
-    const GOOD = 200
-    const NOT_FOUND = 404
+    //const baseURL = 'https://reqres.in/api'
+
     test('Simple API Test - Assert Response Status', async ({ request }) => {
-        const response = await request.get(`${rootURL}/users/2`)
+        const response = await request.get(`${baseURL}/users/2`)
         expect(response.status()).toBe(GOOD)
+        const responseBody = JSON.parse(await response.text())
+        console.log(responseBody)
     })
 
     test('Simple API Test - Assert Invalid Endpoint', async ({ request }) => {
-        const response = await request.get(`${rootURL}/users/non-existant-user`)
+        const response = await request.get(`${baseURL}/users/non-existant-user`)
         expect(response.status()).toBe(NOT_FOUND)
+    })
+
+    test('Simple API Test - Get User Detail', async ({ request }) => {
+        const response = await request.get(`${baseURL}/users/1`)
+        const responseBody = JSON.parse(await response.text())
+        expect(response.status()).toBe(200)
+        expect(responseBody.data.id).toBe(1)
+        expect(responseBody.data.first_name).toBe('George')
+        expect(responseBody.data.last_name).toBe('Bluth')
+        expect(responseBody.data.email).toBeTruthy()
     })
 })
