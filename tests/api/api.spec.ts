70c303c6b28bc78815dd9c3ba3b793a815147169
Adam <algolec1990@yahoo.com>
2023-06-05 22:46:43 -0400

    add post request for successful login, failed login (no email) and failed login (no password)

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -29,7 +29,7 @@ test.describe.parallel('API Testing', () => {
         expect(responseBody.data.last_name).toBe('Bluth')
         expect(responseBody.data.email).toBeTruthy()
     })
-    test.only('POST Request - Create New User', async ({ request }) => {
+    test('POST Request - Create New User', async ({ request }) => {
         const ID = 1000
         const response = await request.post(`${baseURL}/users`, {
             data: {
@@ -39,5 +39,45 @@ test.describe.parallel('API Testing', () => {
         const responseBody = JSON.parse(await response.text())
         expect(responseBody.id).toBe(ID)
         expect(responseBody.createdAt).toBeTruthy()
+    })
+
+    test('POST Request - Login with correct + valid credentials', async ({
+        request,
+    }) => {
+        const response = await request.post(`${baseURL}/login`, {
+            data: {
+                email: 'eve.holt@reqres.in',
+                password: 'cityslicka',
+            },
+        })
+        const responseBody = JSON.parse(await response.text())
+        expect(response.status()).toBe(200)
+        expect(responseBody.token).toBeTruthy()
+    })
+
+    test.only('POST Request - Login FAIL: Wrong Email Address', async ({
+        request,
+    }) => {
+        const response = await request.post(`${baseURL}/login`, {
+            data: {
+                password: 'cityslicka',
+            },
+        })
+        const responseBody = JSON.parse(await response.text())
+        expect(response.status()).toBe(400)
+        expect(responseBody.error).toBeTruthy()
+    })
+
+    test.only('POST Request - Login Fail - Invalid Password', async ({
+        request,
+    }) => {
+        const response = await request.post(`${baseURL}/login`, {
+            data: {
+                email: 'eve.holt@reqres.in',
+            },
+        })
+        const responseBody = JSON.parse(await response.text())
+        expect(response.status()).toBe(400)
+        expect(responseBody.error).toBeTruthy()
     })
 })
