49c558539b6178d14ff327ce85e261cd3ed15582
Adam Golec <algolec1990@yahoo.com>
2025-10-05 12:52:38 -0400

    add skip tests

--- tests/tips/tips.spec.ts
+++ tests/tips/tips.spec.ts
@@ -1,8 +1,18 @@
 import {test,expect} from '@playwright/test'
 
+//testInfo will tell you a lot of information about the test you are running. It will give 
+//you access to a lot of different pieces of information inside of it.
 test.describe.only("Tips & Tricks Section", () => {
     test("TestInfo Object", async ({page}, testInfo ) => {
         await page.goto('https://www.example.com')
         console.log(testInfo.expectedStatus)
     })
+
+    test("Test Skip Browser", async ({page, browserName}) =>{
+        //use test.skip to skip a test on a certain browser.
+        test.skip(browserName === 'chromium', 'feature not ready in chrome browser')
+        await page.goto('https://www.example.com')
+    })
 })
+
+
