8fd575aabe1fae9b98844e4176e68fbb3975e24f
Adam Golec <algolec1990@yahoo.com>
2025-10-05 12:57:10 -0400

    add FixMe annotation

--- tests/tips/tips.spec.ts
+++ tests/tips/tips.spec.ts
@@ -2,7 +2,7 @@ import {test,expect} from '@playwright/test'
 
 //testInfo will tell you a lot of information about the test you are running. It will give 
 //you access to a lot of different pieces of information inside of it.
-test.describe.only("Tips & Tricks Section", () => {
+test.describe("Tips & Tricks Section", () => {
     test("TestInfo Object", async ({page}, testInfo ) => {
         await page.goto('https://www.example.com')
         console.log(testInfo.expectedStatus)
@@ -11,6 +11,13 @@ test.describe.only("Tips & Tricks Section", () => {
     test("Test Skip Browser", async ({page, browserName}) =>{
         //use test.skip to skip a test on a certain browser.
         test.skip(browserName === 'chromium', 'feature not ready in chrome browser')
+        await page.goto('https://www.example.com')
+    })
+
+    //indicates a problem with test code. Not the application itself.
+    test("Test FixMe Annotation", async ({page, browserName}) =>{
+        //use test.skip to skip a test on a certain browser.
+        test.fixme(browserName === 'chromium', 'test is not stable. Needs revision at a later sprint')
         await page.goto('https://www.example.com')
     })
 })
